92941de17aaac692b44fdb6704ba351fb71d839f
Your Name <you@example.com>
2018-03-24 19:39:41 -0400

    ajax call with recipe id

--- assets/js/auth.js
+++ assets/js/auth.js
@@ -74,16 +74,44 @@ firebase.auth().onAuthStateChanged(firebaseUser => {
 
         //listen for clicks on the 'heart' button to save the recipe in favorites
         $(".favorite").on("click", function () {
-        
-        //get the recipe id of the liked item and push it to the database
-            recipeId: this.attr(recipe - Id);
+
+            //get the recipe id of the liked item and push it to the database
+            recipeId: this.attr(recipe-Id);
             database.ref(userId).push(
                 recipeId
             );
         })
-
     } else {
         console.log('not logged in')
         userId = "";
     }
+})
+
+//Listen for fav being added to the database.  
+database.ref(userId).on("child_added", function(childSnapshot){
+    //take the snapshot and write to the DOM
+    console.log(childSnapshot.val());
+
+function getRecipeById(id) {
+    //  take in the id and return the recipe
+    var apiKey = "NaJ8IatR4umshJBKw1RZRU7m6EnQp1QfWPajsnjxYr5FbYb8Gv";
+    var url =
+      "https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/";
+    url +=
+      id +
+      "/information?" +
+      $.param({
+        includeNutrition: "true" //Include nutrition data to the recipe information. Nutrition data is per serving. If you want the nutrition data for the entire recipe, just multiply by the number of servings.
+      });
+  
+    $.ajax({
+      url: url,
+      method: "GET",
+      headers: {
+        "X-Mashape-Key": apiKey
+      }
+    }).then(function(response) {
+      showRecipeInModal(response);
+    });
+  }
 })
